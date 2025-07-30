0c2100b675e63ea4c2a607673024fc86b3d87fef
Marcin Szymczak <marcin.szymczak@dolby.com>
2025-07-30 13:18:56 +0000

    nvim context

--- home-manager/tools/pyenv.nix
+++ home-manager/tools/pyenv.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ settings, ... }:
 {
   programs.pyenv = {
     enable = true;
     enableZshIntegration = true;
+    rootDirectory =
+      if settings.system == "x86_64-linux" then
+        "/home/${settings.username}/.pyenv"
+      else
+        "/Users/${settings.username}/.pyenv";
   };
 }
