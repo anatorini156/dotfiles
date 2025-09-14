54070d705e38df734d2f3438efeab1db8047eaf5
anatorini <marcin.szymczak156@gmail.com>
2025-09-14 14:16:45 +0200

    NixOS Sun Sep 14 02:16:45 PM CEST 2025

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -22,6 +22,10 @@
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
     uwsm = {
       enable = true;
       waylandCompositors = {
