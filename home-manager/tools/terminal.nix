f286e283e695ae6fcc86132e1bd0dda38c400d70
Marcin Szymczak <marcin.szymczak@dolby.com>
2025-09-15 14:20:42 +0200

    Home-manager Mon Sep 15 14:20:42 CEST 2025

--- home-manager/tools/terminal.nix
+++ home-manager/tools/terminal.nix
@@ -21,7 +21,7 @@
       "alt+4" = "goto_tab 4";
       "alt+5" = "goto_tab 5";
     };
-    themeFile = "rose-pine-moon";
+    themeFile = "rose-pine";
     settings = {
       tab_bar_style = "powerline";
       tab_powerline_style = "round";
