1a1aa3e613686c57542e9f48c93568b015a0e63f
anatorini <marcin.szymczak156@gmail.com>
2025-09-24 19:51:27 +0200

    Home-manager Wed Sep 24 07:51:27 PM CEST 2025

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -10,7 +10,7 @@
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
