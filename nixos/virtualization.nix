b50daf0f30d218109ca39cfab9f561e6970be145
anatorini <marcin.szymczak156@gmail.com>
2025-10-02 19:14:04 +0200

    NixOS Thu Oct  2 07:14:04 PM CEST 2025

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -1,3 +1,16 @@
-{pkgs,...} : {
-virtualization = {};
-    }
+{ pkgs, ... }:
+{
+  virtualization = {
+    virtualbox = {
+      host = {
+        enable = true;
+        enableExtensionPack = true;
+      };
+      guest = {
+        enable = true;
+        dragAndDrop = true;
+      };
+    };
+    users.extraGroups.vboxusers.members = [ "anatorini" ];
+  };
+}
