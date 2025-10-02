a6047c0dc90f6dc4f29f6226d8fa64c12261eb33
anatorini <marcin.szymczak156@gmail.com>
2025-10-02 19:09:17 +0200

    disable tree

--- nvim/lua/plugins/jumptree.lua
+++ nvim/lua/plugins/jumptree.lua
@@ -1,7 +1,7 @@
 return {
-  "anatorini156/jumptree",
-  dev = true,
-  config = function ()
-    require("anatorini156.jumptree").setup()
-  end
+  -- "anatorini156/jumptree",
+  -- dev = true,
+  -- config = function ()
+  --   require("anatorini156.jumptree").setup()
+  -- end
 }
