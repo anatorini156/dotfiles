529f5fcd2319e574ce72689994f0e1d40362fdaf
anatorini <marcin.szymczak156@gmail.com>
2025-10-04 17:53:33 +0200

    Home-manager Sat Oct  4 05:53:33 PM CEST 2025

---
+++ home-manager/hosts/ThinkPad-X1.nix
@@ -0,0 +1,29 @@
+{
+  username,
+  pkgs,
+  zen,
+  ...
+}:
+{
+  home.packages = with pkgs; [
+    spotify
+    discord
+    gimp3
+    flameshot
+    zen.packages.${pkgs.system}.default
+    tidal-hifi
+    unstable.jetbrains.idea-community-bin
+    jetbrains.clion
+
+    gcc
+    chafa
+    alsa-utils
+    xclip
+    zen.packages.${pkgs.system}.default
+    reaper
+    coolercontrol.coolercontrold
+    coolercontrol.coolercontrol-gui
+    libreoffice-still
+    postman
+  ];
+}
