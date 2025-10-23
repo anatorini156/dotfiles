14a5ec5e6fbd55b55537d9db20da1f41d17a1aa0
anatorini <marcin.szymczak156@gmail.com>
2025-10-23 22:14:32 +0200

    NixOS Thu Oct 23 10:14:32 PM CEST 2025

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,15 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
+    hyprland = {
+      type = "git";
+      url = "https://github.com/hyprwm/Hyprland";
+      submodules = true;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    Hyprspace = {
+      url = "github:KZDKM/Hyprspace";
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations."ThinkPad-X1" = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-        ./hosts/ThinkPad-X1/hardware-configuration.nix
-        ./hosts/ThinkPad-X1/configuration.nix
-      ];
+      # Hyprspace uses latest Hyprland. We declare this to keep them in sync.
+      inputs.hyprland.follows = "hyprland";
     };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      hyprland,
+      Hyprspace,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations."ThinkPad-X1" = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          ./hosts/ThinkPad-X1/hardware-configuration.nix
+          ./hosts/ThinkPad-X1/configuration.nix
+        ];
+      };
+    };
 }
