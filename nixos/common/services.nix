55e29c8e54b45eb30e61ba49f1f8e950a04cee7f
anatorini <marcin.szymczak156@gmail.com>
2025-10-24 20:11:10 +0200

    NixOS Fri Oct 24 08:11:10 PM CEST 2025

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -6,19 +6,18 @@
       pkgs.gnome-keyring
       pkgs.gcr
     ];
+    displayManager = {
+        sddm = {
+            enable = true;
+            wayland = {
+                enable = true;
+            };
+        };
+    };
     fprintd.enable = true;
     fprintd.tod.enable = true;
     gnome.gnome-keyring.enable = true;
     fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
-    xserver = {
-      enable = true;
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-      xkb = {
-        layout = "pl";
-        variant = "";
-      };
-    };
     printing = {
       enable = true;
     };
