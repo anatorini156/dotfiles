fe008a2946a8b9fc1b3e6f16684b88ca28f2d9c7
Marcin Szymczak <marcin.szymczak@dolby.com>
2026-02-16 11:31:53 +0100

    colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,17 +4,26 @@ return {
 		name = "rose-pine",
 		priority = 512,
 	},
-  {
-    "loctvl842/monokai-pro.nvim",
-    name = "monokai-pro",
-    priority = 2048,
-    config = function()
-      vim.cmd("colorscheme rose-pine-main")
-    end,
-  },
+	{
+		"loctvl842/monokai-pro.nvim",
+		name = "monokai-pro",
+		priority = 2048,
+	},
 	{
 		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1024,
+		name = "catppuccin-frappe",
+		priority = 256,
+		-- config = function()
+		-- 	vim.cmd("colorscheme catppuccin-frappe")
+		-- end,
+	},
+	{
+		"nkxxll/ghostty-default-style-dark.nvim",
+		lazy = false,
+		priority = 128,
+		config = function()
+			require("ghostty-default-style-dark").setup({})
+			vim.cmd.colorscheme("ghostty-default-style-dark")
+		end,
 	},
 }
